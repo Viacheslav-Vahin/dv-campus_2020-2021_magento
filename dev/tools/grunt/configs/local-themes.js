4d100444c9fc53dac3f9dd42b601c543a9d95f71
Viacheslav <vahin.vvv@gmail.com>
2020-12-01 13:26:18 +0200

    #7 Blocks and templates: Apply `uk_UA` or locale on the main website.

--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -1,8 +1,19 @@
 /**
  * grunt exec:viacheslav_luma_en_us && grunt less:viacheslav_luma_en_us && grunt watch
+ * grunt exec:viacheslav_luma_uk_ua && grunt less:viacheslav_luma_uk_ua && grunt watch
  */
 
 module.exports = {
+    viacheslav_luma_uk_ua: {
+        area: 'frontend',
+        name: 'Viacheslav/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     viacheslav_luma_en_us: {
         area: 'frontend',
         name: 'Viacheslav/luma',
